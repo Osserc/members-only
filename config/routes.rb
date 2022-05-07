237ac60a40ea8a9a3051dd19c0a8e49fe92ac726
Stefano Rondelli <rstefanofil@hotmail.com>
2022-05-07 18:34:32 +0200

    Re-did it all again - starting point

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :stories
+  devise_for :users, :controllers => { registrations: 'registrations'}
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "stories#index"
+end
